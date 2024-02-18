9f258501b8614bff686baf327fd4fcdb3f2bee89
Mete Vesek <metevesekk@hotmail.com>
2024-02-18 17:04:15 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Mete Vesek on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
